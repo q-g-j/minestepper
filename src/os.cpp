0b94d244efc1c0eaabfc3d8cf92607b5e3322d29
QGJ <qgj@gmx.net>
2020-05-04 11:22:46 +0200

    added 2 files to git

---
+++ src/os.cpp
@@ -0,0 +1,29 @@
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+    #include "windows.h"  
+#endif
+
+#include "os.hpp"
+
+bool OS::isWindows()
+{
+    bool isWindowsBool = false;
+    #if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+        isWindowsBool = true;
+    #endif
+    return isWindowsBool;
+}
+    
+bool OS::isWine()
+{
+    bool isWineBool = false;
+    if (isWindows())
+    {      
+        HKEY hKey;
+        LPCSTR lpRegPath = ("SOFTWARE\\Wine");
+        if (RegOpenKeyExA(HKEY_CURRENT_USER, lpRegPath, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
+            isWineBool = true;
+        return isWineBool;
+    }
+    else 
+        return false;
+}
---
+++ src/os.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class OS
+{
+private:
+
+public:
+    bool isWindows();
+    bool isWine();
+};
